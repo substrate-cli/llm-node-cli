7c3b4a2f6e0072290213a050876a7255ca66ecdb
siriuss <shreyash@192.168.1.6>
2025-10-12 15:16:26 +0530

    prevent-restart

--- webhooks/callApiWebhook.js
+++ webhooks/callApiWebhook.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { getAPIServerUrl } from "../utils/configuration.js";
 
-export async function errorHandler(status, errorMessage, message) {
+export async function errorHandler(status, errorMessage, message, isRetry=false) {
     const payload = {
 	  "ARCHIVE_KEY": "streamchat",
 	  "type":        "error",
 	  "stream":      errorMessage,
 	  "status":      status,
 	  "error":       message,
+    "isRetry":     isRetry
 	}
-
     let response;
     let url = `${getAPIServerUrl()}/api/webhook/error`
     try {
